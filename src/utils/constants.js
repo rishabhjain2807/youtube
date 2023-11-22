38218cf4bac093b64bd18bf17ef25c9705b13500
Rishabhjain <jain.rishabh2807@gmail.com>
2023-11-22 15:24:28 +0530

    221123

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,7 @@
 const GOOGLE_API_KEY = "AIzaSyB-mEBATceGQeDIYR1uVZ8QeY2Yx66BAJo";
+
+export const OFFSET_LIVE_COUNT = 25;
+
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
